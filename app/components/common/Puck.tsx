6baa8fa06703d8cdde029231dd727f28a5e9e0c0
Kyle Tan <kylehanlin.tan@uhn.ca>
2025-06-23 10:43:33 -0400

    add layout for chatbot puck

---
+++ app/components/common/Puck.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+// i need it to open up a side modal
+export default function Puck() {
+  return (
+    <>
+      <div className="rounded">
+        <div></div>
+      </div>
+    </>
+  );
+}
