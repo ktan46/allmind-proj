91f62b25eeba664d9cb50c013a25acb87d17722c
Kyle Tan <kylehanlin.tan@uhn.ca>
2025-06-23 10:44:37 -0400

    add section text

--- app/components/feature/landingpage/Features.tsx
+++ app/components/feature/landingpage/Features.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 
 export default function Features() {
-  return <div>Features</div>;
+  return (
+    <section className="bg-white text-black text-center">
+      <h2>Augment Your Firm with an AI Platform Built for Finance</h2>
+    </section>
+  );
 }
