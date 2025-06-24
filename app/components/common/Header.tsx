6b967cd357045a5c1ee75159b3f68069a44aefef
Kyle Tan <kylehanlin.tan@uhn.ca>
2025-06-24 16:46:12 -0400

    adjust the header logo size

--- app/components/common/Header.tsx
+++ app/components/common/Header.tsx
@@ -9,12 +9,7 @@ export default function Header() {
   return (
     <header className="sticky top-0 z-50 w-full px-16 py-4 flex items-center bg-transparent backdrop-blur-md border-b border-gray-400">
       <Link href="/">
-        <Image
-          src="/images/icons/vercel.svg"
-          alt="logo"
-          width={24}
-          height={24}
-        />
+        <Image src="/images/icons/Logo.svg" alt="logo" width={60} height={24} />
       </Link>
       <nav className="flex flex-1 justify-center space-x-6">
         {navItems.map((item) => (
