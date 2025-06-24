1bd4b0cf94fd15e23c2d5f880ecd187f6c573250
Kyle Tan <kylehanlin.tan@uhn.ca>
2025-06-24 19:12:29 -0400

    add id to component

--- app/components/feature/landingpage/HeroBanner.tsx
+++ app/components/feature/landingpage/HeroBanner.tsx
@@ -17,13 +17,16 @@ export default function HeroBanner({
   scrollingBanner,
 }: HeroBannerProps) {
   return (
-    <section className="-mt-18 relative w-full h-screen bg-[url('/images/main-hero.jpg')] bg-cover bg-center bg-no-repeat bg-fixed px-6 text-center">
+    <section
+      id="hero"
+      className="-mt-18 relative w-full h-screen bg-[url('/images/main-hero.jpg')] bg-cover bg-center bg-no-repeat bg-fixed px-6 text-center"
+    >
       <div className="absolute inset-0 bg-black/40 backdrop-blur-sm">
         <div className="relative z-10 max-w-4xl mx-auto py-84 text-white">
           <h1 className="text-2xl md:text-7xl font-bold mb-4">{title}</h1>
           <h2 className="text-2xl mb-6">{subtitle}</h2>
           <div className="mb-10">
-            <BaseButton>{buttonName}</BaseButton>
+            <BaseButton className="bg-white">{buttonName}</BaseButton>
           </div>
           <h3 className="text-gray-300">{bannerDescription}</h3>
           {scrollingBanner && (
