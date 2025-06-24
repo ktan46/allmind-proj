1a7c8a03e9f0722b469c1ba8d50275b2834a9742
Kyle Tan <kylehanlin.tan@uhn.ca>
2025-06-24 19:12:39 -0400

    update button styling

--- app/components/common/BaseButton.tsx
+++ app/components/common/BaseButton.tsx
@@ -3,13 +3,23 @@ import React from "react";
 type BaseButtonProps = {
   children: React.ReactNode;
   onClick?: () => void;
+  className?: string;
 };
 
-export default function BaseButton({ children, onClick }: BaseButtonProps) {
+export default function BaseButton({
+  children,
+  onClick,
+  className = "",
+}: BaseButtonProps) {
   return (
     <button
       onClick={onClick}
-      className="bg-white text-black rounded-4xl font-medium px-4 py-2 hover:bg-gray-300 cursor-pointer transition-colors duration-400"
+      className={`
+        text-black rounded-4xl font-medium
+        px-4 py-2 hover:bg-gray-300
+        cursor-pointer transition-colors duration-400
+        ${className}
+      `}
     >
       {children}
     </button>
